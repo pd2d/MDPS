f07cbb1bbe1455f04814da585ab0eab8203ff8f4
pd2d <96042094+pd2d@users.noreply.github.com>
2022-11-28 08:01:20 +0530

    Add files via upload

--- md.py
+++ md.py
@@ -8,11 +8,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/USER/OneDrive/Desktop/MDPS/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('C:/Users/USER/Downloads/MDPS/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/USER/OneDrive/Desktop/MDPS/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('C:/Users/USER/Downloads/MDPS/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/USER/OneDrive/Desktop/MDPS/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('C:/Users/USER/Downloads/MDPS/parkinsons_model.sav', 'rb'))
 
 
 
